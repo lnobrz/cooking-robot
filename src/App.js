5f9ee63fa25732bdd7999d5ae0cd9f5a1506436e
Fernandes <jose.fernandes@hp.com>
2022-10-28 13:26:44 -0300

    update app.js to render new components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Header from "./components/Header";
+import Cover from "./components/Cover";
+import DeliveryProcess from "./components/DeliveryProcess";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Header />
+      <Cover />
+      <DeliveryProcess />
+    </div>
+  );
 }
 
 export default App;
