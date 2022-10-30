af380e2fd3a3669c62c7bed3aeaa1b920417be4a
Fernandes <jose.fernandes@hp.com>
2022-10-30 02:53:07 -0300

    add MenuItems component to Menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,12 @@
 import MenuCategories from "./MenuCategories";
+import MenuItems from "./MenuItems";
 
 const Menu = () => {
   return (
     <div className="flex flex-col justify-center items-center bg-lightRed pt-14 pb-28">
       <h2 className="font-display text-3xl text-center mt-12">Menu</h2>
       <MenuCategories />
+      <MenuItems />
     </div>
   );
 };
