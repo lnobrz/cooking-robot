d32fb59870b9f89751590491c1c4bfcd16b6f992
Lucas Nobre <lucasnobre37@outlook.com>
2022-10-31 12:30:52 -0300

    render statistics dinamically

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -24,35 +24,21 @@ const Statistics = () => {
   ];
 
   return (
-    <div className="w-9/12 bg-softWhite flex flex-row justify-evenly items-center rounded-lg shadow-lg absolute -top-72 py-14 z-10 m-auto left-0 right-0">
-      <div>
-        <div className="flex flex-row justify-center items-center">
-          <img src="/assets/icons/green-elipse.svg" alt="green circle" />
-          <p className="font-display text-2xl text-center ml-2.5">
-            COMPLETED ORDERS
+    <ul className="w-9/12 bg-softWhite flex flex-row justify-evenly items-center rounded-lg shadow-lg absolute -top-72 py-14 z-10 m-auto left-0 right-0">
+      {statisticsData.map((statistic) => (
+        <li key={statistic.key}>
+          <div className="flex flex-row justify-center items-center">
+            <img src={statistic.elipseIcon} alt={statistic.iconDescription} />
+            <p className="font-display text-2xl text-center ml-2.5">
+              {statistic.name.toUpperCase()}
+            </p>
+          </div>
+          <p className="font-display text-6xl text-center mt-2.5">
+            {statistic.amount.toUpperCase()}
           </p>
-        </div>
-        <p className="font-display text-6xl text-center mt-2.5">987</p>
-      </div>
-      <div>
-        <div className="flex flex-row justify-center items-center">
-          <img src="/assets/icons/green-elipse.svg" alt="green circle" />
-          <p className="font-display text-2xl text-center ml-2.5">
-            SATISFIED CLIENTS
-          </p>
-        </div>
-        <p className="font-display text-6xl text-center mt-2.5">99.3%</p>
-      </div>
-      <div>
-        <div className="flex flex-row justify-center items-center">
-          <img src="/assets/icons/yellow-elipse.svg" alt="yellow circle" />
-          <p className="font-display text-2xl text-center ml-2.5">
-            AWARDS OWNED
-          </p>
-        </div>
-        <p className="font-display text-6xl text-center mt-2.5">99.3%</p>
-      </div>
-    </div>
+        </li>
+      ))}
+    </ul>
   );
 };
 
