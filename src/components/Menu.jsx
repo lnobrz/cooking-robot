74cd56b52c801715fca62b37f3a69f235496d60b
Lucas Nobre <lucasnobre37@outlook.com>
2022-10-30 22:23:06 -0300

    pass necessary props to MenuCategories and MenuItems component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -439,8 +439,8 @@ const Menu = () => {
   return (
     <div className="flex flex-col justify-center items-center bg-lightRed pt-14 pb-28">
       <h2 className="font-display text-3xl text-center mt-12">Menu</h2>
-      <MenuCategories />
-      <MenuItems />
+      <MenuCategories items={items} filteredItemsSetter={setFilteredItems} />
+      <MenuItems items={filteredItems} />
     </div>
   );
 };
