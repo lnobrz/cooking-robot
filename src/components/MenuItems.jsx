2a9a75c3ee6b30c5052d3db7b6ce77ee045b7912
Lucas Nobre <lucasnobre37@outlook.com>
2022-11-01 00:33:19 -0300

    add MenuItem component to MenuItems

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import MenuItem from "../MenuItem";
 
 const MenuItems = (props) => {
   const [isMenuFullyOpen, setIsMenuFullyOpen] = useState(false);
@@ -17,30 +18,12 @@ const MenuItems = (props) => {
         id="menuItemsContainer"
       >
         {props.items.map((item) => (
-          <li
-            className="w-72 h-60	grid grid-cols-12 grid-rows-6 gap-x-1"
-            key={item.name}
-          >
-            <img
-              src={item.image}
-              alt={item.imageDescription}
-              className="w-72 h-48 max-w-full max-h-full bg-cover	col-span-full	row-start-1 row-end-5"
-            />
-            <p className="font-display font-black mt-1 col-span-full row-start-5 row-end-5">
-              {item.name}
-            </p>
-            <p className="text-base font-display font-black text-brightRed col-start-1 col-end-3 row-start-6 row-end-6">
-              ${item.price}
-            </p>
-            <div className="flex flex-row justify-between px-1 py-1 items-center w-11 h-7 border border-grey shadow-lg col-start-9 col-end-10 row-start-6 row-end-6">
-              <button>-</button>
-              <p>1</p>
-              <button>+</button>
-            </div>
-            <button className="bg-brightRed p-1 h-7 w-full rounded-sm col-start-11 col-end-13 row-start-6 row-end-6 flex justify-center items-center">
-              <img src="/assets/icons/cart-icon-2.svg" alt="cart icon" />
-            </button>
-          </li>
+          <MenuItem
+            name={item.name}
+            image={item.image}
+            imageDescription={item.imageDescription}
+            price={item.price}
+          />
         ))}
       </ul>
       <button
