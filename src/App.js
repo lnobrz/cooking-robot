585f5723805236d5db27e14c6ab57e3854ded35d
Lucas Nobre <lucasnobre37@outlook.com>
2022-11-01 01:26:42 -0300

    pass the global context to App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import Header from "./components/Header";
 import Cover from "./components/Cover";
 import DeliveryProcess from "./components/DeliveryProcess";
 import Menu from "./components/Menu";
+import { CartStorage } from "./storage/cart";
 
 function App() {
   return (
-    <>
+    <CartStorage>
       <Header />
       <Cover />
       <DeliveryProcess />
       <Menu />
-    </>
+    </CartStorage>
   );
 }
 
