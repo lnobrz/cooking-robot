d7adccc8bd7d97b2d2d4b1e2c6e7f7e8cb708207
Lucas Nobre <lucasnobre37@outlook.com>
2022-11-01 00:36:45 -0300

    use title component on Cover component

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,10 +1,10 @@
+import { DisplayTitle } from "./ui/Titles";
+
 const Cover = () => {
   return (
     <div className="flex flex-row justify-between items-center bg-lightRed pt-14 pb-28">
       <div className="container w-2/4">
-        <h1 className="text-6xl font-display font-black mx-16">
-          We mix food and technology to deliver the best to your home.
-        </h1>
+        <DisplayTitle title="We mix food and technology to deliver the best to your home." />
         <button className="flex flex-row justify-center items-center w-64 h-16 rounded-lg bg-brightRed mx-16 mt-9 text-white font-text mb-16">
           <p>ORDER NOW</p>
           <img
