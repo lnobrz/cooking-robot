e8a1ad89ce2d152b2379dd25c11007c7ea2bd078
Lucas Nobre <lucasnobre37@outlook.com>
2022-11-07 22:44:56 -0300

    add CartModal component to header and create a state to check if modal should be open

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,36 @@
+import CartModal from "./CartModal";
+import { useState } from "react";
+import ReactDOM from "react-dom";
+
 const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleCartClick = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
-    <nav className="h-28 flex flex-row justify-between items-center bg-lightRed">
-      <img
-        src="/assets/images/logo.png"
-        alt="cooking robot's logo"
-        className="w-20 h-20 ml-16"
-      />
-      <img
-        src="/assets/icons/cart-icon.svg"
-        alt="shopping cart icon"
-        className="w-8 h-8 mr-16"
-      />
-    </nav>
+    <>
+      {isModalOpen &&
+        ReactDOM.createPortal(
+          <CartModal closeBtnFunc={handleCartClick} />,
+          document.getElementById("cartModal")
+        )}
+      <nav className="h-28 flex flex-row justify-between items-center bg-lightRed">
+        <img
+          src="/assets/images/logo.png"
+          alt="cooking robot's logo"
+          className="w-20 h-20 ml-16"
+        />
+        <button onClick={handleCartClick}>
+          <img
+            src="/assets/icons/cart-icon.svg"
+            alt="shopping cart icon"
+            className="w-8 h-8 mr-16"
+          />
+        </button>
+      </nav>
+    </>
   );
 };
 
