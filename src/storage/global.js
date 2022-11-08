47b51ada3bb8b08a0da90f150355e24ed026c2a0
Lucas Nobre <lucasnobre37@outlook.com>
2022-11-08 12:03:09 -0300

    exclude finalPrice global var

--- src/storage/global.js
+++ src/storage/global.js
@@ -5,12 +5,9 @@ export const GlobalContext = createContext();
 export const GlobalStorage = ({ children }) => {
   const [cart, setCart] = useState([]);
   const [amount, setAmount] = useState("");
-  const [finalPrice, setFinalPrice] = useState(0);
 
   return (
-    <GlobalContext.Provider
-      value={{ cart, setCart, amount, setAmount, finalPrice, setFinalPrice }}
-    >
+    <GlobalContext.Provider value={{ cart, setCart, amount, setAmount }}>
       {children}
     </GlobalContext.Provider>
   );
