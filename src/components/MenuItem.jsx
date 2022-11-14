3cee8b9919c9ee242e48908bb4e198d1b67ead85
Lucas Nobre <lucasnobre37@outlook.com>
2022-11-14 18:53:10 -0300

    get MenuItem code clearer

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -49,20 +49,20 @@ const MenuItem = (props) => {
   return (
     <li
       className="w-72 h-60	grid grid-cols-12 grid-rows-6 gap-x-1"
-      key={props.name}
+      key={props.item.name}
     >
       <img
-        src={props.image}
-        alt={props.imageDescription}
+        src={props.item.image}
+        alt={props.item.imageDescription}
         className="w-72 h-48 max-w-full max-h-full bg-cover	col-span-full	row-start-1 row-end-5"
       />
       <SmallTitle
-        title={props.name}
+        title={props.item.name}
         additionalClasses="mt-1 col-span-full row-start-5 row-end-5"
       />
       <SmallTitle
         preText="$"
-        title={props.price}
+        title={props.item.price}
         additionalClasses="text-brightRed col-start-1 col-end-3 row-start-6 row-end-6"
       />
       <AmountInput
@@ -73,9 +73,7 @@ const MenuItem = (props) => {
       />
       <button
         className="bg-brightRed p-1 h-7 w-full rounded-sm col-start-11 col-end-13 row-start-6 row-end-6 flex justify-center items-center"
-        onClick={(event) =>
-          handleCartBtnClick(props.id, props.name, props.price)
-        }
+        onClick={(event) => handleCartBtnClick(props.item)}
       >
         <img src="/assets/icons/cart-icon-2.svg" alt="cart icon" />
       </button>
