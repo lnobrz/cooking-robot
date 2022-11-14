0e76a05eea493258539e82d92dc02fa131a48745
Lucas Nobre <lucasnobre37@outlook.com>
2022-11-14 18:38:22 -0300

    update global storage to handle browser's local storage

--- src/storage/global.js
+++ src/storage/global.js
@@ -3,11 +3,18 @@ import { createContext, useState } from "react";
 export const GlobalContext = createContext();
 
 export const GlobalStorage = ({ children }) => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(
+    JSON.parse(localStorage.getItem("userCart"))
+      ? JSON.parse(localStorage.getItem("userCart"))
+      : []
+  );
   const [amount, setAmount] = useState("");
+  const localStorageCart = JSON.parse(localStorage.getItem("userCart"));
 
   return (
-    <GlobalContext.Provider value={{ cart, setCart, amount, setAmount }}>
+    <GlobalContext.Provider
+      value={{ cart, setCart, amount, setAmount, localStorageCart }}
+    >
       {children}
     </GlobalContext.Provider>
   );
