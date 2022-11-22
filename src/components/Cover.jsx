fd3064f60b9b84d73ce3cfda0d4406815f1f689a
Lucas Nobre <lucasnobre37@outlook.com>
2022-11-22 19:28:18 -0300

    add OrderNow handleClick function

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,6 +1,11 @@
 import { DisplayTitle } from "./ui/Titles";
 
 const Cover = () => {
+
+  const scrollToMenu = () => {
+    window.location.replace("/#menu")
+  }
+
   return (
     <div className="flex flex-row flex-wrap-reverse justify-center lg:justify-between lg:flex-nowrap bg-lightRed pt-14 pb-28">
       <div className="mx-16 text-center flex flex-col items-center justify-center lg:w-2/4 lg:justify-start lg:items-start lg:text-left lg:mx-0">
@@ -8,7 +13,7 @@ const Cover = () => {
           title="We mix food and technology to deliver the best to your home."
           additionalClasses="w-full md:w-4/6"
         />
-        <button className="flex flex-row justify-center items-center w-64 h-16 rounded-lg bg-brightRed md:mx-16 mt-9 text-white font-text mb-16">
+        <button className="flex flex-row justify-center items-center w-64 h-16 rounded-lg bg-brightRed md:mx-16 mt-9 text-white font-text mb-16" onClick={scrollToMenu}>
           <p>ORDER NOW</p>
           <img
             src="./assets/icons/arrow-right.svg"
