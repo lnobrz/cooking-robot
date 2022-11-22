f17ff3c3bf76eecdc3b7093593d106ce210c96da
Lucas Nobre <lucasnobre37@outlook.com>
2022-11-22 19:20:34 -0300

    revert fixed position of CartButton

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -13,13 +13,13 @@ const CartButton = (props) => {
       : 0;
 
   return (
-    <button onClick={props.onClick} className="border-2 border-black		fixed flex items-center justify-center w-20 h-20 mr-16 rounded -right-10 bottom-10 bg-lightRed z-50">
+    <button onClick={props.onClick} className="relative w-10 h-10 mr-16">
       <img
         src="./assets/icons/cart-icon.svg"
         alt="shopping cart icon"
         className="h-10"
       />
-      <div className="bg-brightRed w-6 h-6 rounded-full absolute top-0 -right-0">
+      <div className="bg-brightRed w-6 h-6 rounded-full absolute -top-6 -right-3">
         <SmallTitle title={totalAmount} additionalClasses="text-softWhite" />
       </div>
     </button>
