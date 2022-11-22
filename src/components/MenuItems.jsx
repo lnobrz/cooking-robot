29dbb02b1656225fcd07124ad8395795bada85d7
Lucas Nobre <lucasnobre37@outlook.com>
2022-11-21 21:32:50 -0300

    add responsivity + update code to handle http requests errors

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { SmallTitle, BigTitle } from "./ui/Titles";
 import MenuItem from "./MenuItem";
 
 const MenuItems = (props) => {
+  const [loading, setLoading] = useState(true);
   const [menuFullyOpen, setMenuFullyOpen] = useState(false);
   const container = document.getElementById("menuItemsContainer");
+
+  useEffect(() => {
+    if (props.items) {
+      setLoading(false)
+    }
+  }, [props.items])
 
   const menuFullyOpenToggle = () => {
     setMenuFullyOpen((previousValue) => !previousValue);
@@ -11,17 +19,18 @@ const MenuItems = (props) => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center rounded-lg bg-softWhite mx-16 my-16">
+    <div className="flex flex-col justify-center items-center rounded-lg bg-softWhite m-16 p-16">
       <ul
-        className="w-full flex justify-center max-h-534 overflow-hidden	items-start flex-wrap gap-7 mx-12 mt-16"
+        className="flex justify-center items-center flex-wrap max-h-534 overflow-hidden gap-7"
         id="menuItemsContainer"
-      >
-        {props.items.map((item) => (
+      >{loading ? <SmallTitle title="loading..." additionalClasses="mt-9" /> : (Array.isArray(props.items) ?
+        props.items.map((item) => (
           <MenuItem item={item} />
-        ))}
+        ))
+        : <BigTitle title={props.items} additionalClasses="text-brightRed mt-9" />)}
       </ul>
-      <button
-        className="w-52 h-16 rounded-lg flex justify-center items-center bg-brightRed font-text font-black text-white mt-10 mb-16"
+      {!loading && Array.isArray(props.items) && <button
+        className="w-52 h-16 rounded-lg flex justify-center items-center 3xl:hidden bg-brightRed font-text font-black text-white mt-8"
         onClick={menuFullyOpenToggle}
       >
         <p className="mr-1">{menuFullyOpen ? "view less" : "view more"}</p>
@@ -37,7 +46,7 @@ const MenuItems = (props) => {
               : "arrow poiting down bellow"
           }
         />
-      </button>
+      </button>}
     </div>
   );
 };
