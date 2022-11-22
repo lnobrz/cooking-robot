0f91e3489c2e309f5609732406f7f078ce69a88d
Lucas Nobre <lucasnobre37@outlook.com>
2022-11-22 20:45:07 -0300

    remove order now button

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -2,24 +2,14 @@ import { DisplayTitle } from "./ui/Titles";
 
 const Cover = () => {
 
-  const scrollToMenu = () => {
-    window.location.replace("/#menu")
-  }
-
   return (
-    <div className="flex flex-row flex-wrap-reverse justify-center lg:justify-between lg:flex-nowrap bg-lightRed pt-14 pb-28">
-      <div className="mx-16 text-center flex flex-col items-center justify-center lg:w-2/4 lg:justify-start lg:items-start lg:text-left lg:mx-0">
-        <DisplayTitle
-          title="We mix food and technology to deliver the best to your home."
-          additionalClasses="w-full md:w-4/6"
-        />
-        <button className="flex flex-row justify-center items-center w-64 h-16 rounded-lg bg-brightRed md:mx-16 mt-9 text-white font-text mb-16" onClick={scrollToMenu}>
-          <p>ORDER NOW</p>
-          <img
-            src="./assets/icons/arrow-right.svg"
-            alt="arrow poiting to the right"
-          />
-        </button>
+    <div className="flex flex-col-reverse justify-center lg:flex-row lg:justify-between lg:flex-nowrap bg-lightRed pt-14 pb-28 text-center items-center lg:items-start lg:text-left lg:mx-0  mb-16">
+      <DisplayTitle
+        title="We mix food and technology to deliver the best to your home."
+        additionalClasses="w-10/12 xsm:w-8/12 sm:w-7/12 md:w-7/12 lg:w-6/12 xl:w-4/12 2xl:w-3/12"
+      />
+      <div className="">
+
       </div>
       <img
         src="./assets/images/cover-image.png"
